cf010df2cf89074915869d0509c7810a7dc71688
umairamubashshsera <umairahamidi09@gmail.com>
2024-05-20 01:56:09 +0530

    soomcar all file

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+    $("document").ready(function() {
+		$("#video").simplePlayer();
+    });
+    
+     $("document").ready(function() {
+		$("#video1").simplePlayer();
+    });
+    
+    
+});
+
+
+jQuery("#carousel").owlCarousel({
+  autoplay: true,
+  lazyLoad: true,
+  loop: true,
+  margin: 20,
+   /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 7000,
+  smartSpeed: 800,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+
+    600: {
+      items: 3
+    },
+
+    1024: {
+      items: 4
+    },
+
+    1366: {
+      items: 4
+    }
+  }
+});
+
